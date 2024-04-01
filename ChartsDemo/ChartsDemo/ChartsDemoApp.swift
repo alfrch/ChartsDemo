51676b30465f997da1356bb18b9e13d6555063ef
alfrch <alfrchdev@gmail.com>
2024-04-01 14:24:26 +0700

    add initial project

---
+++ ChartsDemo/ChartsDemo/ChartsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsDemoApp.swift
+//  ChartsDemo
+//
+//  Created by Alif R on 01/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ChartsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
