63c9bf3f35f4eeaed7d79242bbc48e407f54e2f4
alfrch <alfrchdev@gmail.com>
2024-04-01 16:43:13 +0700

    setup picker with graph type

--- ChartsDemo/ChartsDemo/View/Home.swift
+++ ChartsDemo/ChartsDemo/View/Home.swift
@@ -10,10 +10,22 @@ import SwiftUI
 struct Home: View {
     /// View Properties
     @State private var graphType: GraphType = .donut
+    
     var body: some View {
         VStack {
+            /// Segmented Picker
+            Picker("", selection: $graphType) {
+                ForEach(GraphType.allCases, id: \.rawValue) { type in
+                    Text(type.rawValue)
+                        .tag(type)
+                }
+            }
+            .pickerStyle(.segmented)
+            .labelsHidden()
             
+            Spacer(minLength: 0)
         }
+        .padding()
     }
 }
 
