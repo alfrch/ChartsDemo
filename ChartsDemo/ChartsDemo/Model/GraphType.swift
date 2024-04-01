360bc16362172fa7cc44d461c825d97b05549786
alfrch <alfrchdev@gmail.com>
2024-04-01 15:27:12 +0700

    conform GraphType to String and CaseIterable

--- ChartsDemo/ChartsDemo/Model/GraphType.swift
+++ ChartsDemo/ChartsDemo/Model/GraphType.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-enum GraphType {
-    case bar
-    case pie
-    case donut
+enum GraphType: String, CaseIterable {
+    case bar = "Bar Chart"
+    case pie = "Pie Chart"
+    case donut = "Donut Chart"
 }
