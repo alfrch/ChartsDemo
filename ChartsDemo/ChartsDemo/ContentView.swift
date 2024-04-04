7cdd57c759bbe381fd8f3a5a1f1079e6e5d1efa0
alfrch <alfrchdev@gmail.com>
2024-04-04 14:08:32 +0700

    gajels

--- ChartsDemo/ChartsDemo/ContentView.swift
+++ ChartsDemo/ChartsDemo/ContentView.swift
@@ -6,19 +6,46 @@
 //
 
 import SwiftUI
+import Charts
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                ForEach(MockData.plannedTasks) { sprint in
+                    BarMark(
+                        x: .value("Phase", sprint.phase),
+                        y: .value("abc", sprint.numberOfTasks)
+                    )
+                }
+            }
+            .navigationTitle("Sprint Tracker")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct Sprint: Identifiable {
+    var id: UUID = .init()
+    var phase: String
+    var numberOfTasks: Int
+}
+
+struct MockData {
+    static let plannedTasks: [Sprint] = [
+        .init(phase: "Planning", numberOfTasks: 8),
+        .init(phase: "Development", numberOfTasks: 15),
+        .init(phase: "Testing", numberOfTasks: 10),
+        .init(phase: "Deployment", numberOfTasks: 5),
+    ]
+    
+    static let completedTasks: [Sprint] = [
+        .init(phase: "Planning", numberOfTasks: 7),
+        .init(phase: "Development", numberOfTasks: 10),
+        .init(phase: "Testing", numberOfTasks: 5),
+        .init(phase: "Deployment", numberOfTasks: 5),
+    ]
 }
